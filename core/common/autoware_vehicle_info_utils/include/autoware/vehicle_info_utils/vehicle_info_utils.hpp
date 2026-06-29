a3afca228ca48b8970964660a247cde1601f374c
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2026-06-29 19:59:15 +0900

    feat(VehicleInfoUtils): templatize NodeT of `VehicleInfoUtils` (tier4/autoware_core#1223)
    * templatize VehicleInfoUtils
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    * revert unnecessary chages
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    (cherry picked from commit fe75d66416d41d45961f6bcf2f04617dcbb21c5e)

--- core/common/autoware_vehicle_info_utils/include/autoware/vehicle_info_utils/vehicle_info_utils.hpp
+++ core/common/autoware_vehicle_info_utils/include/autoware/vehicle_info_utils/vehicle_info_utils.hpp
@@ -17,6 +17,7 @@
 
 #include "autoware/vehicle_info_utils/vehicle_info.hpp"
 
+#include <autoware_utils_rclcpp/parameter.hpp>
 #include <rclcpp/node.hpp>
 
 namespace autoware::vehicle_info_utils
@@ -29,7 +30,36 @@ class VehicleInfoUtils
 public:
   /// Constructor
   // NOTE(soblin): this throws which should be replaced with a factory
-  explicit VehicleInfoUtils(rclcpp::Node & node);
+  template <typename NodeT>
+  explicit VehicleInfoUtils(NodeT & node)
+  {
+    static constexpr const char * WHEEL_RADIUS = "wheel_radius";
+    static constexpr const char * WHEEL_WIDTH = "wheel_width";
+    static constexpr const char * WHEEL_BASE = "wheel_base";
+    static constexpr const char * WHEEL_TREAD = "wheel_tread";
+    static constexpr const char * FRONT_OVERHANG = "front_overhang";
+    static constexpr const char * REAR_OVERHANG = "rear_overhang";
+    static constexpr const char * LEFT_OVERHANG = "left_overhang";
+    static constexpr const char * RIGHT_OVERHANG = "right_overhang";
+    static constexpr const char * VEHICLE_HEIGHT = "vehicle_height";
+    static constexpr const char * MAX_STEER_ANGLE = "max_steer_angle";
+
+    using autoware_utils_rclcpp::get_or_declare_parameter;
+    const auto wheel_radius_m = get_or_declare_parameter<double>(node, WHEEL_RADIUS);
+    const auto wheel_width_m = get_or_declare_parameter<double>(node, WHEEL_WIDTH);
+    const auto wheel_base_m = get_or_declare_parameter<double>(node, WHEEL_BASE);
+    const auto wheel_tread_m = get_or_declare_parameter<double>(node, WHEEL_TREAD);
+    const auto front_overhang_m = get_or_declare_parameter<double>(node, FRONT_OVERHANG);
+    const auto rear_overhang_m = get_or_declare_parameter<double>(node, REAR_OVERHANG);
+    const auto left_overhang_m = get_or_declare_parameter<double>(node, LEFT_OVERHANG);
+    const auto right_overhang_m = get_or_declare_parameter<double>(node, RIGHT_OVERHANG);
+    const auto vehicle_height_m = get_or_declare_parameter<double>(node, VEHICLE_HEIGHT);
+    const auto max_steer_angle_rad = get_or_declare_parameter<double>(node, MAX_STEER_ANGLE);
+
+    vehicle_info_ = createVehicleInfo(
+      wheel_radius_m, wheel_width_m, wheel_base_m, wheel_tread_m, front_overhang_m, rear_overhang_m,
+      left_overhang_m, right_overhang_m, vehicle_height_m, max_steer_angle_rad);
+  }
 
   /// Get vehicle info
   [[nodiscard]] VehicleInfo getVehicleInfo() const;
