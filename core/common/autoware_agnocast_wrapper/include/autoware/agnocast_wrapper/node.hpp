b1dd8cb669c820184b1071fd3289af4d86a82145
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2026-07-21 19:23:13 +0900

    fix(autoware_agnocast_wrapper): `declare_paramter` return type to auto (autowarefoundation/autoware_core#1278)
    fix declare_paramter return type to auto
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- core/common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/node.hpp
+++ core/common/autoware_agnocast_wrapper/include/autoware/agnocast_wrapper/node.hpp
@@ -144,15 +144,19 @@ public:
 
   // ===== Parameters (template) =====
   template <typename ParameterT>
-  ParameterT declare_parameter(
+  auto declare_parameter(
     const std::string & name, const ParameterT & default_value,
     const rcl_interfaces::msg::ParameterDescriptor & descriptor =
       rcl_interfaces::msg::ParameterDescriptor{},
     bool ignore_override = false)
   {
-    return declare_parameter(
-             name, rclcpp::ParameterValue(default_value), descriptor, ignore_override)
-      .get<ParameterT>();
+    try {
+      return declare_parameter(
+               name, rclcpp::ParameterValue(default_value), descriptor, ignore_override)
+        .get<ParameterT>();
+    } catch (const rclcpp::ParameterTypeException & ex) {
+      throw rclcpp::exceptions::InvalidParameterTypeException(name, ex.what());
+    }
   }
 
   template <typename ParameterT>
@@ -603,15 +607,19 @@ public:
 
   // ===== Parameters (template) =====
   template <typename ParameterT>
-  ParameterT declare_parameter(
+  auto declare_parameter(
     const std::string & name, const ParameterT & default_value,
     const rcl_interfaces::msg::ParameterDescriptor & descriptor =
       rcl_interfaces::msg::ParameterDescriptor{},
     bool ignore_override = false)
   {
-    return declare_parameter(
-             name, rclcpp::ParameterValue(default_value), descriptor, ignore_override)
-      .get<ParameterT>();
+    try {
+      return declare_parameter(
+               name, rclcpp::ParameterValue(default_value), descriptor, ignore_override)
+        .get<ParameterT>();
+    } catch (const rclcpp::ParameterTypeException & ex) {
+      throw rclcpp::exceptions::InvalidParameterTypeException(name, ex.what());
+    }
   }
 
   template <typename ParameterT>
